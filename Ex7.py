f281208b1e42f266b329444498e93685e10553e6
Andresa Lidia <Andresa Ldia>
2022-12-11 14:44:36 -0400

    adicionando Ex7

---
+++ Ex7.py
@@ -0,0 +1,5 @@
+altura = float(input("Digite a altura da caixa:"))
+largura = float(input("Digite a largura da caixa:"))
+cumprimento = float(input("Digite o cumprimento da caixa:"))
+volume = altura * largura * cumprimento
+print ("O volume da caixa é:",volume)
