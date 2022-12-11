040cf0702be2b5c3774387053a87daefc0a23d68
Andresa Lidia <Andresa Ldia>
2022-12-11 15:21:49 -0400

    adicionando Ex10

---
+++ Ex10.py
@@ -0,0 +1,4 @@
+reais = float(input("Informe quantos reais você deseja converter:"))
+dolar = float(input("Informe a cotação de dolar atual:"))
+convercao = reais/dolar
+print("R$",reais,"é igual a:", dolar,"US$")
