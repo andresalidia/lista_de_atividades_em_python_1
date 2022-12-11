c579888f3525dd4e1537e17d573e335f0f310348
Andresa Lidia <Andresa Ldia>
2022-12-11 13:41:50 -0400

    adicionando Ex5

---
+++ Ex5.py
@@ -0,0 +1,7 @@
+valorA = int(input("Digite um valor A:"))
+valorB = int (input("Digite um valor B:"))
+aux = valorA
+valorA = valorB
+valorB = aux
+print("O valor A é:", valorA)
+print("O valor B é:", valorB)
