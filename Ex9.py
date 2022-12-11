0973d062e232af807465a4a0f9c9de88005c87bf
Andresa Lidia <Andresa Ldia>
2022-12-11 14:59:42 -0400

    adicionando Ex9

---
+++ Ex9.py
@@ -0,0 +1,4 @@
+valorA = int(input("Digite um valor:"))
+valorB = int(input("Digite um outro valor:"))
+resultado = (valorA - valorB)**2
+print("O quadrado da diferença de", valorA,"e",valorB,"é igual a", resultado)
