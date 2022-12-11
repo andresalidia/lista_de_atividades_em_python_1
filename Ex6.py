fda39c025811fb8872e4b5a49f3911c3251d7f29
Andresa Lidia <Andresa Ldia>
2022-12-11 14:39:04 -0400

    adicionando Ex6

---
+++ Ex6.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Digite um valor:"))
+valor2 = int(input("Digite um valor:"))
+valor3 = int(input("Digite um valor:"))
+valor4 = int(input("Digite um valor:"))
+soma = valor1 + valor2 + valor3 + valor4
+multiplicacao = valor1 * valor2 * valor3 * valor4
+print("A soma dos valore é", soma)
+print("A multiplicação dos valores é:", multiplicacao)
